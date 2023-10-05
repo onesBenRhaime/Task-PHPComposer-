dccf1ee4285ee7a449ff894ca78db821ebeded1b
onesBenRhaime <onesrhaime28@gmail.com>
2023-10-05 11:44:01 +0100

    adding CSS

--- index.php
+++ index.php
@@ -1,34 +1,48 @@
+
 <?php
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use GuzzleHttp\Client;
+
 $client = new Client();
-$response = $client->get('https://dummyjson.com/data');
 
-$data = json_decode($response->getBody(), true);
+try {
+    $response = $client->get('https://dummyjson.com/products');
 
+    $data = json_decode($response->getBody(), true);
+
+    if (!empty($data['products'])) {
+        
+        ?>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Product List</title>
+            <link rel="stylesheet" href="styles.css">
+        </head>
+
+        <body>
+            <ul class="product-list">
+                <?php foreach ($data['products'] as $product) : ?>
+                    <li class="product-item">
+                        <img src="<?php echo $product['thumbnail']; ?>" alt="<?php echo $product['title']; ?>">
+                        <h2><?php echo $product['title']; ?></h2>
+                        <p><?php echo $product['description']; ?></p>
+                        <p>$<?php echo $product['price']; ?></p>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </body>
+
+        </html>
+        <?php
+    } else {
+        echo 'Aucun produit disponible.';
+    }
+} catch (Exception $e) {
+    echo 'Erreur: ' . $e->getMessage();
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product List</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-
-<body>
-    <ul class="product-list">
-        <?php foreach ($data['products'] as $product) : ?>
-            <li class="product-item">
-                <img src="<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>">
-                <h2><?php echo $product['name']; ?></h2>
-                <p><?php echo $product['description']; ?></p>
-                <p>$<?php echo $product['price']; ?></p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-
-</html>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -29,8 +29,8 @@ class ComposerStaticInit6ee02805929cdc4170808665d4dac34c
     public static $prefixDirsPsr4 = array (
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
